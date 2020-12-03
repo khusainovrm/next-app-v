b8256c32fb59f98c54665d56e59482137f793588
Rinat Khusainov <khusainovrm@yandex.ru>
2020-12-03 16:51:34 +0300

    refactor: dynamic routes

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -1,10 +1,14 @@
 import { getAllPostIds } from '../../lib/getAllPostIds'
 import { getPostById } from '../../lib/getPostById'
 import Layout from '../../components/layout'
+import Head from 'next/head'
 
 export default function Post({ postData }) {
   return (
     <Layout>
+      <Head>
+        <title>{postData.title}</title>
+      </Head>
       <div>title: {postData.title}</div>
       <div>ID: {postData.id}</div>
     </Layout>
