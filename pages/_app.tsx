38aedab75a3186ea630c0a0fbfab22434802214a
Rinat Khusainov <khusainovrm@yandex.ru>
2020-12-04 10:34:19 +0300

    feat: progress loader

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,17 @@
 import '../styles/global.css'
 import { AppProps } from 'next/app'
+import NextNprogress from 'nextjs-progressbar'
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <NextNprogress
+        color="#29D"
+        startPosition={0.3}
+        stopDelayMs={200}
+        height="3"
+      />
+      <Component {...pageProps} />
+    </>
+  )
 }
